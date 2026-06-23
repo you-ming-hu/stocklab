2c977d044c87560eaa9720eb41b70c45d28376eb
youming <atlas0435@gmail.com>
2026-06-24 01:35:12 +0800

    update institution flow individual stock check

--- playground/initiate/checker/institution_flow/twse.py
+++ playground/initiate/checker/institution_flow/twse.py
@@ -10,7 +10,11 @@ class TWSEChecker(Checker):
         return content
     
 class STOCKS(TWSEChecker):
-    pass
+    def standardize(self, c):
+        data_name = 'data'
+        if data_name in c:
+            c[data_name] = {v[0]:v for v in c[data_name]}
+        return c
 
 class MARKET(TWSEChecker):
     def standardize(self, c):
