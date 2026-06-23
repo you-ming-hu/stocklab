64c8be88cf59498e8464528cc7fa96022a7bd545
youming <atlas0435@gmail.com>
2026-06-24 01:35:47 +0800

    add foreign ownership

---
+++ playground/initiate/checker/foreign_ownership/twse.py
@@ -0,0 +1,16 @@
+import json
+
+from .. import Checker
+
+class TWSEChecker(Checker):
+    
+    def read(self, path):
+        with open(path, encoding='utf-8') as f:
+            content = json.load(f)
+        return content
+    
+class STOCKS(TWSEChecker):
+    def standardize(self, c):
+        return c
+
+stocks = STOCKS()
