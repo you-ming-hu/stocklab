a07adfc6e8ffb470ef8e5cdd60bf43edb63aec6f
youming <atlas0435@gmail.com>
2026-07-22 15:36:37 +0800

    update check method

--- playground/initiate/checker/otc/margin_short/all/stocks_volume_market_volume_value.py
+++ playground/initiate/checker/otc/margin_short/all/stocks_volume_market_volume_value.py
@@ -1,9 +1,25 @@
+import re
+
 from ...base import OTCChecker
 
 class VERSION_0(OTCChecker):
     
     def read(self, path):
         return path.read_text(encoding='utf-8')
+    
+    def standardize(self, c):
+        c = re.sub(r'<script>.*?</script>', '', c)
+        return c
+    
+    def remove_inconsistent_files(self, save_path, t1, t2):
+        keys, target1, target2 = self.get_targets(save_path, t1, t2)
+        diff = self.read_diff(save_path)
+        print(f'theoretical remove count: {len(diff)}')
+        for d in diff:
+            for target in [target1[d], target2[d]]:
+                if '嚙' in target.read_text(encoding='utf-8'):
+                    target.unlink()
+                    print(f'removed: {target}')
     
 version_0 = VERSION_0()
 
