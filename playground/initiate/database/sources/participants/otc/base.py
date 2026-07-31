1982dfecb203878068b02b7e9119913cbb61c3aa
youming <atlas0435@gmail.com>
2026-07-31 12:25:25 +0800

    add otc institutional flow value

--- playground/initiate/database/sources/participants/otc/base.py
+++ playground/initiate/database/sources/participants/otc/base.py
@@ -10,7 +10,6 @@ class FLOW_VOLUME(Source):
             if name in table:
                 target_table = table
         return len(target_table['data']) == 0
-        # return len(content['tables'][0]['data']) == 0
     
     def to_df(self, content, name='totalCount'):
         target_table = None
@@ -50,25 +49,34 @@ class FLOW_VOLUME(Source):
 #             df[name] = df[name].astype(float)
 #         return df
 
-# class FLOW_VALUE(Source):
-    
-#     def check_empty(self, content):
-#         return content['stat'] == '很抱歉，沒有符合條件的資料!'
+class FLOW_VALUE(Source):
+    def check_empty(self, content):
+        return len(content['tables'][0]['data']) == 0
 
-#     def to_df(self, content, column_count):
-#         df = pd.DataFrame(columns=content['fields'], data=content['data']).set_index('單位名稱')
-#         rearrange = {}
-#         for item, values in df.iterrows():
-#             for cate, value in values.items():
-#                 rearrange[item+cate] = value
-#         assert len(rearrange) == column_count
-#         df = pd.Series(rearrange).to_frame().T
-#         return df
+    def to_df(self, content):
+        table = content['tables'][0]
+        df = pd.DataFrame(
+            [r for r in table['data'] if len(r)!=0],
+            columns=table['fields'],
+        )
+        return df
 
-#     def format_dtype(self, df):
-#         for name in df.columns:
-#             df[name] = df[name].str.replace(',','').astype(int)
-#         return df
+    def flat_df(self, df, index_name, column_count):
+        df = df.set_index(index_name)
+        rearrange = {}
+        for item, values in df.iterrows():
+            for cate, value in values.items():
+                rearrange[item.replace('\u3000','')+cate] = value
+        assert len(rearrange) == column_count, len(rearrange)
+        df = pd.Series(rearrange).to_frame().T
+        return df
+
+    def format_dtype(self, df):
+        for name in df.columns:
+            if df[name].dtype == int:
+                continue
+            df[name] = df[name].str.replace(',','').astype(int)
+        return df
 
 class SUM:
     def format_dtype(self, df):
